c1c0238d2819e2140c13fc92eeab8530e2ef9cee
NNA\ABuntsev <abuntsev@nine.com.au>
2019-04-30 16:37:54 +1000

    Updates

---
+++ precache-manifest.5a1f7247268100757f165f42e4d08514.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67842bcf565e9f74ab1f348239824bee",
+    "url": "/react-fancy-components/index.html"
+  },
+  {
+    "revision": "2cfc976c9b67a5c526d8",
+    "url": "/react-fancy-components/static/css/main.3fc6c050.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-fancy-components/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "2cfc976c9b67a5c526d8",
+    "url": "/react-fancy-components/static/js/main.26ce6b86.chunk.js"
+  },
+  {
+    "revision": "2411e48a26ed7d80acc5",
+    "url": "/react-fancy-components/static/js/runtime~main.a472c285.js"
+  }
+]);
